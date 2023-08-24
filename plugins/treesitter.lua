f4482741344eb5f709265e96cb6ef8fe5b0acc41
rick <abneryaoo@gmail.com>
2023-08-24 13:52:37 +0800

    change commenting

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,34 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "json",
+      "javascript",
+      "typescript",
+      "tsx",
+      "yaml",
+      "html",
+      "css",
+      "markdown",
+      "svelte",
+      "graphql",
+      "bash",
+      "lua",
+      "vim",
+      "dockerfile",
+      "gitignore",
     })
+    opts.context_commentstring={
+      enable = true,
+      enable_autocmd = false,
+      config = {
+        javascript = {
+          __default = "// %s",
+          jsx_element = "{/* %s */}",
+          jsx_fragment = "{/* %s */}",
+          jsx_attribute = "// %s",
+          comment = "// %s",
+        },
+      },
+    }
   end,
 }
