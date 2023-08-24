7031ea43033fb8625507bdde2bd650861dcc1dec
rick <abneryaoo@gmail.com>
2023-08-24 10:26:35 +0800

    change theme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "everforest",
+  colorscheme = "catppuccin-frappe",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
