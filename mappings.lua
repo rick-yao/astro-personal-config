566ed7fcc71401db0e0cac8931af3df1223f272b
rick <abneryaoo@gmail.com>
2023-08-29 17:04:05 +0800

    remove ts null-ls action ,add leader lt

--- mappings.lua
+++ mappings.lua
@@ -11,10 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr)
-          require("astronvim.utils.buffer").close(
-            bufnr)
-        end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -23,8 +22,9 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
-    ["<leader>ff"] = { function() require("telescope.builtin").find_files({ hidden = true }) end, desc = "Find files" },
-    ["<leader>ft"] = {":TodoTelescope<cr>", desc = "Find TODO"}
+    ["<leader>ff"] = { function() require("telescope.builtin").find_files { hidden = true } end, desc = "Find files" },
+    ["<leader>ft"] = { ":TodoTelescope<cr>", desc = "Find TODO" },
+    ["<leader>lt"] = { ":TypescriptRemoveUnused<cr>", desc = "TypescriptRemoveUnused" },
   },
   t = {
     -- setting a mapping to false will disable it
