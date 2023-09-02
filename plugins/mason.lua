f6c9d20c38185e6c830fd2c2490572f0e59765c9
rick <abneryaoo@gmail.com>
2023-09-02 14:43:10 +0800

    change mason install , seems not working , ignore

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -14,10 +14,10 @@ return {
         "emmet_ls",
         "eslint",
         "html",
-        -- "js-debug-adapter",
         "jsonls",
         "tailwindcss",
-        "tsserver"
+        "tsserver",
+        "volar"
       })
     end,
   },
@@ -28,7 +28,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
+        -- "volar",
+        "prettier",
         -- "stylua",
       })
     end,
