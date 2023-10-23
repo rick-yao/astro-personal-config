d3be0ad93360dc41e2703c1a58646f4378259052
rick <abneryaoo@gmail.com>
2023-10-23 13:22:46 +0800

    change rust analyzer setting

--- init.lua
+++ init.lua
@@ -54,6 +54,16 @@ return {
       -- "pyright"
     },
     config = {
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+              extraArgs = { "--profile", "rust-analyzer" },
+            },
+          },
+        },
+      },
       tsserver = {
         init_options = {
           preferences = {
