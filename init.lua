9d0138b7aff50ff23159a3d9aa4853685a8397e7
rick <abneryaoo@gmail.com>
2024-01-12 09:17:53 +0800

    fix: seems to fix ts crash

--- init.lua
+++ init.lua
@@ -87,9 +87,9 @@ return {
         },
       },
     },
-    setup_handlers = {
-      tsserver = function(_, opts) require("typescript").setup { server = opts } end,
-    },
+    -- setup_handlers = {
+    --   tsserver = function(_, opts) require("typescript").setup { server = opts } end,
+    -- },
   },
 
   -- Configure require("lazy").setup() options
